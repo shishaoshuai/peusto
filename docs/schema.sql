0c2b7be0fd5251b801c0f4b30bee7bf241f1c6c9
Shi Shao Shuai <shishaoshuai@sina.com>
2013-08-02 16:50:18 +0800

    修改项目SQL

--- docs/schema.sql
+++ docs/schema.sql
@@ -1,23 +1,35 @@
 --
 -- MySQL 5.5.8
--- Wed, 31 Jul 2013 22:00:17 +0000
+-- Fri, 02 Aug 2013 08:49:39 +0000
 --
 
 CREATE DATABASE `peusto` DEFAULT CHARSET utf8;
 
 USE `peusto`;
 
+CREATE TABLE `interest_area` (
+   `idinterest_area` int(11) not null auto_increment,
+   `interest_area_name` varchar(20),
+   `display_order` tinyint(4),
+   PRIMARY KEY (`idinterest_area`),
+   UNIQUE KEY (`interest_area_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=5;
+
+INSERT INTO `interest_area` (`idinterest_area`, `interest_area_name`, `display_order`) VALUES 
+('1', '23', '1'),
+('3', ' ¬“µ', '2'),
+('4', 'º“Õ•', '3');
+
 CREATE TABLE `users` (
    `idusers` int(11) not null auto_increment,
-   `name` varchar(45) not null,
+   `username` varchar(45) not null,
    `password` varchar(45),
    `email` varchar(100),
    `mobile` varchar(45),
-   `register_time` datetime,
+   `register_time` timestamp default CURRENT_TIMESTAMP,
    PRIMARY KEY (`idusers`),
-   UNIQUE KEY (`name`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=5;
+   UNIQUE KEY (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=16;
 
-INSERT INTO `users` (`idusers`, `name`, `password`, `email`, `mobile`, `register_time`) VALUES 
-('1', '0', 'ssssss', '0', '0', ''),
-('4', 'ddd', 'ssssss', 'aaa@bbb.net', '', '');
+INSERT INTO `users` (`idusers`, `username`, `password`, `email`, `mobile`, `register_time`) VALUES 
+('15', 'sss', 'af15d5fdacd5fdfea300e88a8e253e82', 'sss@sss.net', '', '2013-08-02 08:16:33');
