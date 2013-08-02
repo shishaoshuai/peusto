76885ca82e4168c2495a06b482ed3fd2c22911d1
Shi Shao Shuai <shishaoshuai@sina.com>
2013-08-02 21:58:54 +0800

    在数据库脚本中增加target表，user_interest_area表

--- docs/schema.sql
+++ docs/schema.sql
@@ -1,6 +1,6 @@
 --
 -- MySQL 5.5.8
--- Fri, 02 Aug 2013 08:49:39 +0000
+-- Fri, 02 Aug 2013 13:58:28 +0000
 --
 
 CREATE DATABASE `peusto` DEFAULT CHARSET utf8;
@@ -20,6 +20,34 @@ INSERT INTO `interest_area` (`idinterest_area`, `interest_area_name`, `display_o
 ('3', '事业', '2'),
 ('4', '家庭', '3');
 
+CREATE TABLE `target` (
+   `idtarget` int(11) not null auto_increment,
+   `target_name` varchar(140) CHARSET eucjpms,
+   `interest_area` int(11),
+   `priority` int(11),
+   `owner` int(11),
+   PRIMARY KEY (`idtarget`),
+   KEY `FK_targe_ref_interest_area_idx` (`interest_area`),
+   KEY `FK_target_ref_user_idx` (`owner`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 AUTO_INCREMENT=2;
+
+INSERT INTO `target` (`idtarget`, `target_name`, `interest_area`, `priority`, `owner`) VALUES 
+('1', 'sss', '4', '1', '21');
+
+CREATE TABLE `user_interest_area` (
+   `iduser_interest_area` int(11) not null auto_increment,
+   `owner` int(11),
+   `user_interest_area_name` varchar(45),
+   `display_order` tinyint(4),
+   PRIMARY KEY (`iduser_interest_area`),
+   KEY `FK_user_interest_area_ref_users_idx` (`owner`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=7;
+
+INSERT INTO `user_interest_area` (`iduser_interest_area`, `owner`, `user_interest_area_name`, `display_order`) VALUES 
+('4', '21', '23', '1'),
+('5', '21', '事业', '2'),
+('6', '21', '家庭', '3');
+
 CREATE TABLE `users` (
    `idusers` int(11) not null auto_increment,
    `username` varchar(45) not null,
@@ -29,7 +57,7 @@ CREATE TABLE `users` (
    `register_time` timestamp default CURRENT_TIMESTAMP,
    PRIMARY KEY (`idusers`),
    UNIQUE KEY (`username`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=16;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=22;
 
 INSERT INTO `users` (`idusers`, `username`, `password`, `email`, `mobile`, `register_time`) VALUES 
-('15', 'sss', 'af15d5fdacd5fdfea300e88a8e253e82', 'sss@sss.net', '', '2013-08-02 08:16:33');
+('21', 'sss', 'af15d5fdacd5fdfea300e88a8e253e82', 'sss@sss.net', '', '2013-08-02 20:11:53');
